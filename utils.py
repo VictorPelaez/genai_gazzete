bf63e981966a8ab7bb0d08ce35cd33ba50098306
victormanuel.pelaez <victormanuel.pelaez@bbva.com>
2023-08-06 09:30:47 +0200

    update python init

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 import configparser
 import re
 
+
 def readConfig():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -14,3 +15,12 @@ def clear_article(text):
   text = re.sub(r'\W', ' ', text)
   text = re.sub(r'\s+', ' ', text, flags=re.I)
   return text.lower()
+
+def remove_summaries(df):
+  """
+  Description: duplicated, empty summaries and non-specific ones
+  """
+  df.drop_duplicates(subset=['source', 'title'], keep='first', inplace=True)
+  df.drop(df[df["summary"]=="empty"].index, inplace=True)
+  df = df[df['summary'].apply(lambda s: ("llms" in s) | ("llm" in s) | ("generative ai" in s) | ("ai" in s))]  
+  return df
